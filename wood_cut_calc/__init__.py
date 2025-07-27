ca9de1743a0d76a105b5ab9a298d798443cff3b9
David Hooton <david@hooton.org>
2025-07-27 22:21:42 +1000

    feat(package): Initialize Wood Cut Calculator package with version and main module imports
    Signed-off-by: David Hooton <david@hooton.org>

---
+++ wood_cut_calc/__init__.py
@@ -0,0 +1,12 @@
+"""Wood Cut Calculator package.
+
+This package provides tools for optimizing cutting plans for woodworking 
+projects.
+"""
+
+__version__ = "0.1.0"
+
+# Import main modules to make them available from the package
+from . import cutting_algorithms
+from . import svg_generator
+from . import routes
